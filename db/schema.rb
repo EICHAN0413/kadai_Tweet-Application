d1ae74f2d613c302b9b1c7ecf25d68f2ad2be75f
”HIDEKIHIDEKI”HIDEKI <chanike1993@yahoo.co.jp>
2021-07-19 09:23:48 +0900

    postsテーブルのカラムをcontentのみに変更

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,9 @@ ActiveRecord::Schema.define(version: 2021_07_15_023042) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
